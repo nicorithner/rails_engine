a22f95b6acfe7c25c19217dcfc116fa98dfa820a
Nico Rithner <nicorithner@gmail.com>
2020-10-18 18:06:25 -0600

    Correct code using Rubocop cues

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,6 @@
 class Merchant < ApplicationRecord
-  has_many :items
-  has_many :invoices
+  has_many :items, dependent: :destroy
+  has_many :invoices, dependent: :destroy
   has_many :invoice_items, through: :items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
