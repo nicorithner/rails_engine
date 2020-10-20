43316d4e52ffd3fb30ca892808c3c66aa4c9f6bb
Nico Rithner <nicorithner@gmail.com>
2020-10-20 09:34:47 -0600

    Remove relationships from ItemSerializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,4 @@
 class ItemSerializer
   include JSONAPI::Serializer
   attributes :id, :name, :description, :unit_price
-  belongs_to :merchant
-  has_many :invoice_items
-  has_many :invoices, through: :invoice_items
 end
