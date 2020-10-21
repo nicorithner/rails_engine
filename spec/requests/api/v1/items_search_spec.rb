712b38ffc65a0004068b934b9a76367c46db0f2e
Nico Rithner <nicorithner@gmail.com>
2020-10-21 14:41:11 -0600

    Update test

---
+++ spec/requests/api/v1/items_search_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe "search items endpoints" do
+  it 'can find a list of items that contain a fragment, case insensitive' do
+    merchant1 = Merchant.create!({ name: "Schiller" })
+    item1 = merchant1.items.create!({ name: "Haru_1", description: "Item's description",  unit_price: 10.05 })
+    item2 = merchant1.items.create!({ name: "Haru_2", description: "Item's description",  unit_price: 10.05 })
+    item3 = merchant1.items.create!({ name: "Not_It", description: "Item's description",  unit_price: 10.05 })
+
+    get '/api/v1/items/find_all?name=haru'
+    
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    names = json[:data].map do |merchant|
+      merchant[:attributes][:name].downcase
+    end
+
+    expect(names.count).to eq(2)
+    names.each do |name|
+      expect(name).to include('haru')
+    end
+  end
+
+  it 'can find an items that contain a fragment, case insensitive' do
+    merchant1 = Merchant.create!({ name: "Schiller" })
+    item1 = { name: "Haru_1", description: "Item's description", merchant_id: 12345, unit_price: 10.05 }
+    item2 = { name: "Haru_2", description: "Item's description", merchant_id: 12345, unit_price: 10.05 }
+    item3 = { name: "Not_It", description: "Item's description", merchant_id: 12345, unit_price: 10.05 }
+    
+    get '/api/v1/items/find?name=haru'
+    json = JSON.parse(response.body, symbolize_names: true)
+    name = json[:data][:attributes][:name].downcase
+
+    expect(json[:data]).to be_a(Hash)
+    expect(name).to include('haru')
+  end
+end
