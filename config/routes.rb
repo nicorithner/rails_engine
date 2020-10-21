5056b68e03b2dd044ad13322738e0a978329069c
Nico Rithner <nicorithner@gmail.com>
2020-10-21 11:09:39 -0600

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      #=== items
+      get 'items/find_all', to: "items_search#index"
+      get 'items/find', to: "items_search#show"
+      get '/items/:id/merchant', to: 'merchant_items#show'
       resources :items, except: [:new, :edit]
+
+      #=== merchants
+      get 'merchants/find_all', to: "merchant_search#index"
+      get 'merchants/find', to: "merchant_search#show"
+      get '/merchants/:id/items', to: 'merchant_items#index'
       resources :merchants, except: [:new, :edit]
       
-      get '/items/:id/merchant', to: 'merchant_items#show'
-      
-      get '/merchants/:id/items', to: 'merchant_items#index'
     end
   end
 end
