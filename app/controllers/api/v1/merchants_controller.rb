57292a9ec13eef4be16e3db2530ed8e9b1c51802
Nico Rithner <nicorithner@gmail.com>
2020-10-21 15:32:51 -0600

    remove render from destroy and require form private

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -16,11 +16,11 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def destroy
-    render json: MerchantSerializer.new(Merchant.destroy(params[:id]))
+    Merchant.destroy(params[:id])
   end
 
   private
     def merchant_params
-      params.require(:merchant).permit(:id, :name)
+      params.permit(:id, :name)
     end
 end
