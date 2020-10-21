20cbc70c4ad60367bda443729ad4071524a083a1
Nico Rithner <nicorithner@gmail.com>
2020-10-21 15:20:39 -0600

    Create model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,8 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+    def self.find_by_name(name)
+    merchants = Merchant.where("lower(name) LIKE ?", "%" + name.downcase + "%")
+  end
 end
