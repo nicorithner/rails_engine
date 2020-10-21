2dfce6fa44b60db0e8f5d74739ec22fa95a8d8dd
Nico Rithner <nicorithner@gmail.com>
2020-10-21 15:32:19 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
       get 'items/find_all', to: "items_search#index"
       get 'items/find', to: "items_search#show"
       get '/items/:id/merchant', to: 'merchant_items#show'
-      resources :items, except: [:new, :edit]
+      resources :items
 
       #=== merchants
       get 'merchants/find_all', to: "merchant_search#index"
       get 'merchants/find', to: "merchant_search#show"
       get '/merchants/:id/items', to: 'merchant_items#index'
-      resources :merchants, except: [:new, :edit]
+      resources :merchants
       
     end
   end
