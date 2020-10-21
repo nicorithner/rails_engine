ec9d6bd403f71e4c145928931570c62f17602f6e
Nico Rithner <nicorithner@gmail.com>
2020-10-21 15:20:17 -0600

    Update test

--- spec/requests/api/v1/merchant_search_spec.rb
+++ spec/requests/api/v1/merchant_search_spec.rb
@@ -2,22 +2,21 @@ require 'rails_helper'
 describe "search Merchant endpoints" do
   
   it 'can find a list of merchants that contain a fragment, case insensitive' do
-    merchant1 = Merchant.create!({ name: "Schiller" })
-    merchant2 = Merchant.create!({ name: "Barrows and Parker" })
-    merchant3 = Merchant.create!({ name: "Tillman Group" })
-    merchant4 = Merchant.create!({ name: "Williamson Group" })
-    merchant5 = Merchant.create!({ name: "Williamson Group" })
-    merchant6 = Merchant.create!({ name: "Willms and Sons" })
+    Merchant.create!({ name: "Schiller" })
+    Merchant.create!({ name: "Barrows and Parker" })
+    Merchant.create!({ name: "Tillman Group" })
+    Merchant.create!({ name: "Williamson Group" })
+    Merchant.create!({ name: "Williamson Group" })
+    Merchant.create!({ name: "Willms and Sons" })
 
     get '/api/v1/merchants/find_all?name=ILL'
 
-    binding.pry
     json = JSON.parse(response.body, symbolize_names: true)
     names = json[:data].map do |merchant|
       merchant[:attributes][:name]
     end
 
-    expect(names.sort).to eq(["Barrows and Parker", "Schiller", "Tillman Group", "Williamson Group", "Williamson Group", "Willms and Sons"])
+    expect(names.sort).to eq(["Schiller", "Tillman Group", "Williamson Group", "Williamson Group", "Willms and Sons"])
   end
 
   it 'can find a merchants that contain a fragment, case insensitive' do
