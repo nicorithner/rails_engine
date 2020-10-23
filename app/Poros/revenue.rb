4c14a7b668e772d6d1b095c8891d8453b2cb2e2b
Nico Rithner <nicorithner@gmail.com>
2020-10-22 19:54:38 -0600

    Add def initialize and attr_reader

--- app/Poros/revenue.rb
+++ app/Poros/revenue.rb
@@ -1,10 +1,8 @@
 class Revenue
-  
-  def self.date_range(start_date, end_date)
-    # binding.pry
-    Invoice.joins(:invoice_items, :transactions)
-    .merge(Transaction.successful)
-    .where(status: :shipped, created_at: Date.parse(start_date).beginning_of_day..Date.parse(end_date).end_of_day)
-    .sum('invoice_items.quantity * invoice_items.unit_price')
+  attr_reader :id, :revenue
+  def initialize(total)
+    @revenue = total
+    @id = nil
   end
+  
 end
